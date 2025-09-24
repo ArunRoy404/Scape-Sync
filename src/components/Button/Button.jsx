20b64ab30719eeeef24a548d9a624ae4af01858e
ArunRoy404 <156922911+ArunRoy404@users.noreply.github.com>
2025-09-24 21:08:59 +0600

    feat: Button Component Created'

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Button = ({ variant, children, onClick }) => {
+    if (variant === 'primary') {
+        return (
+            <button
+                onClick={onClick}
+                style={{ boxShadow: "0px 8px 16px 0px rgba(57, 164, 50, 0.24)" }}
+                className='active:translate-y-[2px] hover:bg-primary/90 transition-transform duration-300 cursor-pointer bg-primary text-base font-bold rounded-lg text-white px-7 py-3'>
+                {children}
+            </button>
+        );
+    }
+};
+
+export default Button;
