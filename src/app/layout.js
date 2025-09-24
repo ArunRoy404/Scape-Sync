be181af72b41417b168c81d64b7b0c2fa763fc23
ArunRoy404 <156922911+ArunRoy404@users.noreply.github.com>
2025-09-24 20:34:49 +0600

    feat: Initial Project Setup

---
+++ src/app/layout.js
@@ -0,0 +1,30 @@
+import { Public_Sans } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar/Navbar";
+
+
+const publicSans = Public_Sans({
+  variable: "--font-public-sans",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "ScapeSync",
+  description: "Landscaping Management Software",
+   icons: {
+    icon: "/Logo/LogoSingle.png", 
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${publicSans.variable}  antialiased`}
+      >
+        <Navbar/>
+        {children}
+      </body>
+    </html>
+  );
+}
