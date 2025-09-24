a45266d1c36a7f1c60cc8872944187787a0b14b3
ArunRoy404 <156922911+ArunRoy404@users.noreply.github.com>
2025-09-25 02:36:33 +0600

    fix: div to section fixed

--- src/section/FeatureSection.jsx
+++ src/section/FeatureSection.jsx
@@ -4,11 +4,11 @@ import React from 'react';
 
 const FeatureSection = () => {
     return (
-        <div className='pt-20 grid md:grid-cols-2 xl:grid-cols-4 gap-9'>
+        <section className='pt-20 grid md:grid-cols-2 xl:grid-cols-4 gap-9'>
             {
                 featureData.map((feature, index) => <FeatureCard index={index} key={feature.title} feature={feature} />)
             }
-        </div>
+        </section>
     );
 };
 
