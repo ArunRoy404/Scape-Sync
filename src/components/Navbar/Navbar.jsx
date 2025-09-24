48b4995a0ca06361bc1a3ce3bd00256bb71fb47a
ArunRoy404 <156922911+ArunRoy404@users.noreply.github.com>
2025-09-24 21:09:36 +0600

    feat: Navbar created

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import Button from '../Button/Button';
+import Logo from '../UI/Logo';
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className='flex justify-between w-screen container mx-auto border-black py-5'>
+         <Logo  variant='primary'/>
+
+            {/* button container  */}
+            <div>
+                <Button variant='primary'>
+                    Get Started
+                </Button>
+            </div>
+        </nav>
     );
 };
 
