1538cb199c551a0e0ecf649e431563e21e59e82d
ArunRoy404 <156922911+ArunRoy404@users.noreply.github.com>
2025-09-26 03:40:08 +0600

    Register Page added

--- src/components/Register/RegisterForm.jsx
+++ src/components/Register/RegisterForm.jsx
@@ -1,11 +1,79 @@
-import React from 'react';
+"use client";
 
-const RegisterForm = () => {
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+    Form,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import InputCustom from "../Input/InputCustom";
+
+
+const formSchema = z
+    .object({
+        firstName: z.string().min(1, "First name is required"),
+        lastName: z.string().min(1, "Last name is required"),
+        email: z.string().email("Enter a valid email"),
+        password: z.string().min(8, "Password must be at least 8 characters"),
+        confirmPassword: z.string().min(8, "Confirm your password"),
+        agree: z.boolean().refine((val) => val, {
+            message: "You must agree to the terms",
+        }),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+        path: ["confirmPassword"],
+        message: "Passwords do not match",
+    });
+
+
+
+export default function RegisterForm() {
+
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+            confirmPassword: "",
+            agree: false,
+        },
+    });
+
+    function onSubmit(values) {
+        console.log("Form Data:", values);
+    }
+
     return (
-        <div>
-            
-        </div>
-    );
-};
+            <div>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
 
-export default RegisterForm;
+                        {/* input fields */}
+                        <div className="flex items-center gap-4">
+                            <InputCustom form={form} label={'First Name'} id={'firstName'} />
+                            <InputCustom form={form} label={'Last Name'} id={'lastName'} />
+                        </div>
+                        <InputCustom form={form} label={'Email'} id={'email'} />
+                        <InputCustom form={form} label={'Password'} id={'password'} />
+                        <InputCustom form={form} label={'Confirm Password'} id={'confirmPassword'} />
+                        <InputCustom form={form} id={'agree'} checkBox={true} >
+                            I agree to Tech Takes 
+                            <a href="#" className="border-b-1 border-black cursor-pointer">Terms of Service</a> 
+                            and 
+                            <a href="#" className="border-b-1 border-black cursor-pointer">Privacy Policy</a>.
+                        </InputCustom>
+
+
+                        {/* Submit Button */}
+                        <Button type="submit" className="cursor-pointer w-full text-xs md:text-base font-bold leading-6  md:py-6">
+                            Create Account
+                        </Button>
+                    </form>
+                </Form>
+            </div>
+    );
+}
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,5 +1,7 @@
+import GoogleLogIn from '@/components/Authentication/GoogleLogIn';
 import NavbarAuth from '@/components/Navbar/NavbarAuth';
 import RegisterForm from '@/components/Register/RegisterForm';
+import Link from 'next/link';
 import React from 'react';
 
 const RegisterPage = () => {
@@ -8,7 +10,7 @@ const RegisterPage = () => {
             <NavbarAuth />
 
             {/* heading  */}
-            <section className='space-y-2 text-center'>
+            <section className='space-y-2 text-center mb-10 md:mb-16 px-4'>
                 <h1 className='text-[rgba(33,43,54,1)] font-bold text-2xl' >
                     Create your Account
                 </h1>
@@ -17,8 +19,33 @@ const RegisterPage = () => {
                 </p>
             </section>
 
+            <section className='mx-auto max-w-[480px] px-4'>
+                <RegisterForm />
+            </section>
 
-            <RegisterForm />
+
+            <div className="flex items-center mx-auto max-w-[480px] my-3 md:mt-12 md:mb-6 px-4">
+                <div className="flex-grow border-t border-gray-300"></div>
+                <span className="px-4 text-[rgba(99,115,129,1)] text-sm font-normal">OR</span>
+                <div className="flex-grow border-t border-gray-300"></div>
+            </div>
+
+
+            <section className='mx-auto max-w-[480px] px-4'>
+                <GoogleLogIn />
+            </section>
+
+            <section className='mx-auto max-w-[480px] mt-4 md:mt-8 px-4 flex items-center justify-center gap-1'>
+                <p className='text-[rgba(33,43,54,1)] text-sm font-medium'>
+                    Already have an account?
+                </p>
+                <Link
+                    href={'/login'}
+                    className='text-primary font-semibold text-sm'
+                >
+                    Login
+                </Link>
+            </section>
         </main>
     );
 };
