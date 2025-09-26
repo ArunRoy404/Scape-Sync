f4beac3222d940d089aef03874aa6481ec0454e6
ArunRoy404 <156922911+ArunRoy404@users.noreply.github.com>
2025-09-26 16:17:49 +0600

    session added

--- src/components/Role/RoleCard.jsx
+++ src/components/Role/RoleCard.jsx
@@ -6,36 +6,35 @@ import Image from 'next/image';
 import React from 'react';
 
 const RoleCard = ({ role }) => {
-    const {data: session} = useSession()
-    console.log(session?.user?.role);
+    const { data: session } = useSession() || {};
     return (
-            <div
-                className={`p-6 cursor-pointer rounded-2xl border
+        <div
+            className={`p-6 cursor-pointer rounded-2xl border
                         ${role === session?.user?.role ? 'border-primary bg-secondary' : ' bg-gray-100 border-[rgba(223,227,232,1)]  '} space-y-5`}>
 
-                {/* image */}
-                <div className="relative mx-auto w-[100px] h-[100px]">
-                    <Image
-                        src={roleData[role].image}
-                        alt='SpaceSyncLogo'
-                        fill
-                        style={{ objectFit: "contain" }}
-                    />
-                </div>
+            {/* image */}
+            <div className="relative mx-auto w-[100px] h-[100px]">
+                <Image
+                    src={roleData[role].image}
+                    alt='SpaceSyncLogo'
+                    fill
+                    style={{ objectFit: "contain" }}
+                />
+            </div>
 
-                {/* description  */}
-                <div className='gap-2 text-center max-w-[240px] mx-auto'>
-                    <h2 className={`
+            {/* description  */}
+            <div className='gap-2 text-center max-w-[240px] mx-auto'>
+                <h2 className={`
                 ${role === session?.user?.role ? 'text-primary ' : 'text-gray-600 '}
                  text-2xl font-semibold `}>
-                        {roleData[role].role}
-                    </h2>
-                    <p className={`${role === session?.user?.role ? 'text-primary ' : 'text-gray-600 '}
+                    {roleData[role].role}
+                </h2>
+                <p className={`${role === session?.user?.role ? 'text-primary ' : 'text-gray-600 '}
                  text-sm font-semibold `}>
-                        {roleData[role].description}
-                    </p>
-                </div>
+                    {roleData[role].description}
+                </p>
             </div>
+        </div>
     );
 };
 
